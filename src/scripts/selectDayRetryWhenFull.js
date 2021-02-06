f9233e3155bf6bbdbd1df76baa8d47f4acb891af
James Sullivan <jamesfsullivan5@gmail.com>
2021-02-06 16:06:14 -0700

    Add idle timeout check

--- src/scripts/selectDayRetryWhenFull.js
+++ src/scripts/selectDayRetryWhenFull.js
@@ -4,6 +4,13 @@ const selectDayRetryWhenFull = async ({ page, day, timeout }) => {
 	await page.waitForSelector('.passholder_reservations__calendar__day');
 	const btnXpath = `//*[@id="passHolderReservations__wrapper"]/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[4]/button[${day}]`;
 	const isDisabled = (await page.$x(`${btnXpath}[@disabled]`)).length !== 0;
+	const idleBtn = await page.$x('//*[@id="c31_Modal_Component_1"]/div[2]/div/div[2]/button');
+
+	if (idleBtn && idleBtn.length) {
+		logger.debug('Idle timout detected');
+		await idleBtn[0].click();
+		logger.debug('Idele tiemout crisis averted');
+	}
 
 	if (isDisabled) {
 		logger.debug(`Day is full. Running again after ${timeout / 1000 / 60} minutes`);
